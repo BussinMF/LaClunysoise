0211e218e6c473959427c457aff26fd1722ffec1
BussinMF <133649620+BussinMF@users.noreply.github.com>
2023-07-30 01:31:15 +0200

    Final from Nico's repo

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,20 @@
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
+import './app.scss';
 
 // Chargement de la bibliothèque jQuery
 const $ = require('jquery');
 
 // Chargement de la partie JS de bootstrap
 require('bootstrap');
+
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
